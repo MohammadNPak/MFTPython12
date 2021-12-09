4fdb613f5479c5a3229406097d0d7bfc05ba73d5
Teacher <Master@enghelabmft.local>
2021-12-09 18:42:48 +0330

    first phase completed

--- lozi_setare.py
+++ lozi_setare.py
@@ -1,1 +1,9 @@
 n = int(input())
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if j <= i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
