a3d14a858c3c81aad0bdc692bf7a63b385ce2910
Malek Yousefi <57662941+malekyou@users.noreply.github.com>
2021-12-10 00:39:48 +0330

    Update lozi_setare.py
    With great respect to my dear teacher, I made a change in the code just to work with GitHub.

--- lozi_setare.py
+++ lozi_setare.py
@@ -1,10 +1,11 @@
 n = int(input())
-for i in range(n):
-    if i < n//2+1:
-        space = (n - (i*2+1))//2
-        star = (i*2+1)
-        print((space*" " + star*"*" + space*" ")*2)
-    else:
-        space = (i-n//2)
-        star = (n-2*(i-n//2))
-        print((space*" " + star*"*" + space*" ")*2)
+if n%2!=0:
+    for i in range(n):
+        if i < n//2+1:
+            space = (n - (i*2+1))//2
+            star = (i*2+1)
+            print((space*" " + star*"*" + space*" ")*2)
+        else:
+            space = (i-n//2)
+            star = (n-2*(i-n//2))
+            print((space*" " + star*"*" + space*" ")*2)
