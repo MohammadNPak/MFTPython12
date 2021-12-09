f0bc892bbd51da0272a0a83ceb6f80d552f34016
Teacher <Master@enghelabmft.local>
2021-12-09 19:18:54 +0330

    final phase

--- lozi_setare.py
+++ lozi_setare.py
@@ -3,9 +3,8 @@ for i in range(n):
     if i < n//2+1:
         space = (n - (i*2+1))//2
         star = (i*2+1)
-
-        print(space*" " + star*"*" + space*" ")
+        print((space*" " + star*"*" + space*" ")*2)
     else:
         space = (i-n//2)
-        star = (i*2+1)
-        print(space*" " + star*"*" + space*" ")
+        star = (n-2*(i-n//2))
+        print((space*" " + star*"*" + space*" ")*2)
