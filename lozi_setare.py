91f3aa2f190973fbc1b837a0db414090a96f0896
Teacher <Master@enghelabmft.local>
2021-12-09 18:45:21 +0330

    second phase completed

--- lozi_setare.py
+++ lozi_setare.py
@@ -2,8 +2,8 @@ n = int(input())
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        if j <= i:
-            print("*", end="")
-        else:
+        if j <= n-i:
             print(" ", end="")
+        else:
+            print("*", end="")
     print()
